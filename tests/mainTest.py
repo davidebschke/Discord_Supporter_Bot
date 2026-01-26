454525b5f189aa17132eb4abe7a2a286bfea4f0b
David Ebschke <davidebschke8@gmail.com>
2026-01-26 12:33:37 +0100

    Add getGreeting Test

--- tests/mainTest.py
+++ tests/mainTest.py
@@ -1,8 +1,9 @@
 import unittest
-
+from main import getGreeting
 
 class MyTestCase(unittest.TestCase):
-
+    def test_getGreeting(self):
+        assert getGreeting() == "Hello World!"
 
     if __name__ == '__main__':
      unittest.main()
