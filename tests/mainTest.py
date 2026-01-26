c684ae368ff0c3c2d46a5291fd11880493e1fa2c
David Ebschke <davidebschke8@gmail.com>
2026-01-26 12:36:59 +0100

    change getGreeting Test

--- tests/mainTest.py
+++ tests/mainTest.py
@@ -3,7 +3,7 @@ from main import getGreeting
 
 class MyTestCase(unittest.TestCase):
     def test_getGreeting(self):
-        assert getGreeting() == "Hello World!"
+        self.assertEqual( getGreeting(), "Hello World!")
 
     if __name__ == '__main__':
      unittest.main()
