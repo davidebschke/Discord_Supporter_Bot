535e7d6f051fd3c4694204054d0fff80eb5e4ce9
David Ebschke <davidebschke8@gmail.com>
2026-01-27 12:50:12 +0100

    add # pragma: no cover for better deployment testing

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 import threading
 
 # 1. Ein Mini-Webserver für Render
-def run_server():
-    port = int(os.environ.get("PORT", 10000))
-    server = HTTPServer(('0.0.0.0', port), SimpleHTTPRequestHandler)
-    server.serve_forever()
+def run_server(): # pragma: no cover
+    port = int(os.environ.get("PORT", 10000))# pragma: no cover
+    server = HTTPServer(('0.0.0.0', port), SimpleHTTPRequestHandler)# pragma: no cover
+    server.serve_forever()# pragma: no cover
 
-threading.Thread(target=run_server, daemon=True).start()
+threading.Thread(target=run_server, daemon=True).start()# pragma: no cover
 
 # 2. Dein Bot-Logik
-print("Bot startet...")
+print("Bot startet...")# pragma: no cover
 
 
 
