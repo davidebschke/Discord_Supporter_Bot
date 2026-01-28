63e671daed140344a7ee657fe03e885f3e436360
David Ebschke <davidebschke8@gmail.com>
2026-01-28 12:22:53 +0100

    imput welcome_jokes.json for memberJoinSystem.py welcome messages

--- groupFeatures/memberJoinSystem.py
+++ groupFeatures/memberJoinSystem.py
@@ -1,14 +1,17 @@
 import os
-
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 from utils import embeddedMessages
+import json
+import random
 
 intents = discord.Intents.default()
 intents.members = True       # Um zu sehen, wer dem Server beitritt
 intents.voice_states = True  # Um zu sehen, wer in den Voice-Channel geht
 intents.message_content = True
+
+
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 load_dotenv()
@@ -22,19 +25,24 @@ async def on_ready():
 
 @bot.event
 async def on_voice_state_update(member, before, after):
-
-
-   # The first channel you enter
+    if os.path.exists('../assets/welcome_jokes.json'):
+        with open('../assets/welcome_jokes.json', 'r', encoding='utf-8') as f:
+            date_welcome_jokes = json.load(f)
+        random_joke = random.choice(date_welcome_jokes["jokes"]["welcome"])
+    else:
+        print("FEHLER: Path existiert nicht")
+        print(f"Aktueller Arbeitsordner: {os.getcwd()}")
+        random_joke = None
+    # The first channel you enter
     if before.channel is None and after.channel is not None:
         channel = after.channel.guild.system_channel
         if channel:
-            await channel.send(embed=embeddedMessages.embedded_welcome_message(member,after.channel))
+            await channel.send(embed=embeddedMessages.embedded_welcome_message(member,after.channel,random_joke))
 
     # When changing channels
     elif before.channel is not None and after.channel is not None and before.channel != after.channel:
         channel = after.channel.guild.system_channel
         await channel.send(f"🔄 {member.display_name}, ist von `{before.channel.name}` zu `{after.channel.name}` gewechselt.")
-        print(f"Log: {member.name} hat gewechselt.")
 
 # --- 3. START ---
 if __name__ == "__main__":
--- utils/embeddedMessages.py
+++ utils/embeddedMessages.py
@@ -1,10 +1,11 @@
 import datetime
 import discord
 
-def embedded_welcome_message(welcome_member,channel):
+def embedded_welcome_message(welcome_member,channel,description_message):
     embed = discord.Embed(
         title=str(welcome_member) +" hat gerade den Channel "+ str(channel)+" betreten",
-        color=discord.Color.blue()
+        description=str(welcome_member)+" "+description_message,
+        color=discord.Color.brand_green()
     )
     embed.timestamp=datetime.datetime.now(datetime.timezone.utc)
     return embed
