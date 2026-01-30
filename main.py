3663f7460724d704564432a401a1841ff41f7cb5
David Ebschke <davidebschke8@gmail.com>
2026-01-30 18:04:55 +0100

    exclude main.py from sonar

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from discord.ext import commands
-
 from groupFeatures.member_join_system import DiscordToken, bot
 
-if __name__ == "__main__":
+if __name__ == "__main__": # NOSONAR
    if DiscordToken:
        bot.run(DiscordToken)
    else:
