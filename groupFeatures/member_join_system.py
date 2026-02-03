145dc5724822c7bd16596247007873cb086a70b8
David Ebschke <davidebschke8@gmail.com>
2026-02-03 11:24:01 +0100

    add englisch jokes condition in member_join_system.py

--- groupFeatures/member_join_system.py
+++ groupFeatures/member_join_system.py
@@ -51,7 +51,14 @@ async def on_voice_state_update(member, before, after):
         async with await anyio.open_file('assets/welcome_jokes.json', 'r', encoding='utf-8') as file:
             content = await file.read()
             data_welcome_jokes = json.loads(content)
-        random_joke = random.choice(data_welcome_jokes['jokes']['welcome'])
+        local_member_language=member.guild.preferred_locale
+        print(local_member_language)
+
+        if local_member_language == "de-DE":
+            random_joke = random.choice(data_welcome_jokes['jokes']['welcome_DE'])
+        else:
+            random_joke = random.choice(data_welcome_jokes['jokes']['welcome_EN'])
+
     else:
         random_joke = None
         print("der pfad existiert nicht")
