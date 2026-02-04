3548b1989def079550ba96bb64a1aaa9a2501d80
David Ebschke <davidebschke8@gmail.com>
2026-02-04 14:34:31 +0100

    optimize import and code smell #2

--- groupFeatures/console_commands.py
+++ groupFeatures/console_commands.py
@@ -1,10 +1,8 @@
 import json
-import os
-
-import anyio
 import discord
 from discord import app_commands
 from discord.ext import commands
+
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='$', intents=intents)
@@ -36,7 +34,6 @@ class tree_commands(commands.Cog):
             except Exception as e:
                 await interaction.response.send_message(f"Fehler: {e}", ephemeral=True)
             except (json.JSONDecodeError, FileNotFoundError):
-                pass
                 if language.value == "de":
                     await interaction.response.send_message(f"Sprache auf {language.name} gesetzt!", ephemeral=True)
                 elif language.value == "en":
