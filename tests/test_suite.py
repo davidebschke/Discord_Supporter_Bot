4a2938dca99c1a3b9af0a4ed8a93979ee1eb6114
David Ebschke <davidebschke8@gmail.com>
2026-02-05 15:10:26 +0100

    add new tests of treeCommands

--- tests/test_suite.py
+++ tests/test_suite.py
@@ -1,6 +1,7 @@
 import unittest
 
 from tests.utils.test_embedded_messages import Test_embedded_welcome_messages
+from tests.groupFeatures.test_tree_commands import Test_tree_commands
 
 
 def run_suite():
@@ -10,6 +11,7 @@ def run_suite():
     """
     suite = unittest.TestSuite()
     suite.addTests(Test_embedded_welcome_messages)
+    suite.addTests(Test_tree_commands)
     runner = unittest.TextTestRunner()
     runner.run(suite)
 
